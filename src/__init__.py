da22ebc01f4d124d3415fc8bf650b4671ae201be
Jomar Delos Reyes <jomardelosreyes32@gmail.com>
2024-05-30 00:05:50 +0800

    feat: add summary embedding

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,4 @@
 from langchain_community.llms import Ollama
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import DirectoryLoader
 from langchain_community.document_loaders import TextLoader
 from langchain_community.vectorstores import Chroma
@@ -7,21 +6,57 @@ from langchain_community.embeddings import  SentenceTransformerEmbeddings
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 from langchain.prompts import ChatPromptTemplate
+import uuid
+from langchain_core.documents import Document
+from langchain.retrievers.multi_vector import MultiVectorRetriever
+from langchain.storage import InMemoryByteStore
 
 # Load
 loader = DirectoryLoader("./data/raw", glob="./*.txt", loader_cls=TextLoader)
 docs = loader.load()
 
-# Split
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=200)
-splits = text_splitter.split_documents(docs)
+# LLM
+llm = Ollama(model="phi3:mini", temperature=0)
 
-# Embed
+chain = (
+    {"doc": lambda x: x.page_content}
+    | ChatPromptTemplate.from_template("Summarize the following document:\n\n{doc}")
+    | llm
+    | StrOutputParser()
+)
+
+summaries = chain.batch(docs, {"max_concurrency": 5})
+
+print(summaries)
+
+# The vectorstore to use to index the child chunks
 embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
-vectorstore = Chroma.from_documents(documents=splits, embedding=embeddings)
+vectorstore = Chroma(collection_name="summaries", embedding_function=embeddings)
 
-retriever = vectorstore.as_retriever()
-# vectorstore = Chroma.from_documents(documents=splits, embedding=OllamaEmbeddings(model="mistral"), persist_directory="./chroma_dir/")
+# The storage layer for the parent documents
+store = InMemoryByteStore()
+id_key = "doc_id"
+
+# The retriever
+retriever = MultiVectorRetriever(
+    vectorstore=vectorstore,
+    byte_store=store,
+    id_key=id_key,
+)
+doc_ids = [str(uuid.uuid4()) for _ in docs]
+
+# Docs linked to summaries
+summary_docs = [
+    Document(page_content=s, metadata={id_key: doc_ids[i]})
+    for i, s in enumerate(summaries)
+]
+
+# Add
+retriever.vectorstore.add_documents(summary_docs)
+retriever.docstore.mset(list(zip(doc_ids, docs)))
+
+#  Retrieve
+question = "What are the physical characteristics of lamu?"
 
 # Prompt
 template = """Answer the question based only on the following context:
@@ -31,20 +66,17 @@ Question: {question}
 """
 prompt = ChatPromptTemplate.from_template(template)
 
-# LLM
-llm = Ollama(model="phi3:mini", temperature=0)
-
 # Post-processing
 def format_docs(docs):
     return "\n\n".join(doc.page_content for doc in docs)
 
 # Chain
 rag_chain = (
-    { "context" : retriever | format_docs, "question": RunnablePassthrough() }
+    {"context": retriever, "question": RunnablePassthrough()}
     | prompt
     | llm
     | StrOutputParser()
 )
 
-result = rag_chain.invoke("What is the connection between lamu and lang?")
+result = rag_chain.invoke(question)
 print(result)
