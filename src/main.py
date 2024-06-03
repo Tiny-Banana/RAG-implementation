cd94085d48f815e38b127438e1dd6e3a85499ce2
Jomar Delos Reyes <jomardelosreyes32@gmail.com>
2024-06-04 03:09:47 +0800

    feat: add bot api

---
+++ src/main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import HTTPException
+import rag
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class ChatMessage(BaseModel):
+    message: str
+
+@app.get("/")
+async def index():
+    return {"Hello": "World"}
+
+@app.post("/ask")
+async def ask(chat_message: ChatMessage):
+    try:
+        result = rag.answer_query(chat_message.message)
+        return result
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Internal Server Error")
